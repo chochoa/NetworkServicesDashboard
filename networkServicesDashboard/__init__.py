481ab099212a3b503d3d67d528e0c97f1f20f666
Rich Logan <rilogan@cisco.com>
2014-01-14 21:48:04 -0800

    remove newRelic

--- networkServicesDashboard/__init__.py
+++ networkServicesDashboard/__init__.py
@@ -5,9 +5,7 @@ import urllib
 import urllib2
 import siteSetup
 import subprocess
-import newrelic.agent
 
-newrelic.agent.initialize('../newrelic.ini')
 app = Flask(__name__)
 app.config['DEBUG'] = True
 

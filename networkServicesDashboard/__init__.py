b0c8a70602820aefb8ab6edb5e25babedc1c0219
Rich Logan <rich@richlogan.co.uk>
2014-05-31 15:49:07 -0700

    files

--- networkServicesDashboard/__init__.py
+++ networkServicesDashboard/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, request, session, redirect, escape, url_for, Response
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'networkServicesDashboard/static/uploads'
+UPLOAD_FOLDER = '/static/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx'])
 
 app = Flask(__name__)
